427c77cb127ad1919067a23e9977b4568c9cd707
Haim Kastner <haim.kastner@gmail.com>
2022-02-19 20:54:56 +0200

    Add E2E tests

---
+++ e2e/test.spec.ts
@@ -0,0 +1,56 @@
+import puppeteer from 'puppeteer';
+
+let browser: puppeteer.Browser;
+let page: puppeteer.Page;
+
+const FRONT_PORT =  process.env.FRONT_SERVE_PORT || 3001;
+
+export const FRONT_URL = `http://localhost:${FRONT_PORT}`;
+
+console.log(`Running front on ${FRONT_URL}`);
+
+beforeAll(async () => {
+	browser = await puppeteer.launch({ headless: true, devtools: false });
+	page = await browser.newPage();
+	page.on('console', (msg) => console.log(`[${msg.type()}] ${msg.text()}`));
+	await page.setViewport({ width: 1366, height: 768 });
+});
+
+afterAll(async () => {
+	await browser.close();
+});
+
+
+describe('Test App', () => {
+	beforeAll(async () => {
+		await page.goto(`${FRONT_URL}/`);
+	});
+
+	test('Login Page', async () => {
+		await page.waitForSelector('#login-form-container');
+
+		const userInput = await page.$('#email-address-input');
+		await userInput?.focus();
+		await userInput?.type('haim.kastner@gmail.com');
+
+		const passwordInput = await page.$('#login-password');
+		await passwordInput?.focus();
+		await passwordInput?.type('Pass$$rd');
+
+		const submitButton = await page.$('#login-submit');
+		await submitButton?.click();
+
+		await page?.waitForSelector('#minion-grid-box-0', {
+			timeout: 1000 * 60
+		});
+	});
+
+	test('Network Page', async () => {
+		await page.goto(`${FRONT_URL}/#/dashboard/network`);
+		await page?.waitForSelector('#network-page-container', {
+			timeout: 1000 * 60
+		});
+	});
+});
+
+
---
+++ scripts/run-mock-server.js
@@ -0,0 +1,80 @@
+import path from 'path';
+import util from 'util';
+import { exec, spawn } from 'child_process';
+const execPromise = util.promisify(exec);
+
+const prepareOnly = process.argv[2] === '--prepare';
+const runOnly = process.argv[2] === '--run';
+
+
+const BUILD_PATH = process.env.BUILD_PATH || 'temp-e2e';
+const REACT_APP_API_URL = process.env.REACT_APP_API_URL || 'http://localhost:80';
+const MOCK_PORT = process.env.MOCK_PORT || '80';
+const FRONT_SERVE_PORT = process.env.FRONT_SERVE_PORT || '3001';
+
+async function prepareMockServer() {
+
+	try { await execPromise(`mkdir ${path.join('temp-mock')}`); } catch (error) { }
+	try { await execPromise(`cd ${path.join('temp-mock')} && git clone https://github.com/casanet/mock-server.git`); } catch (error) { }
+	try { await execPromise(`cd ${path.join('temp-mock')} && cd mock-server && git pull && npm i`); } catch (error) { }
+
+	try {
+		await execPromise(`npm run build`, {
+			env: {
+				BUILD_PATH,
+				REACT_APP_API_URL,
+			}
+		});
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+async function runMockServer() {
+
+	const server = spawn('node', ['index.js'], {
+		cwd: 'temp-mock/mock-server',
+		env: {
+			PORT: MOCK_PORT
+		}
+	});
+
+	server.stdout.on('data', function (data) {
+		console.log(data.toString());
+	});
+
+	server.stderr.on('data', function (data) {
+		console.log('stderr: ' + data.toString());
+	});
+
+	server.on('exit', function (code) {
+		console.log('child process exited with code ' + code.toString());
+	});
+
+	const fileServer = spawn('node', ['node_modules/http-server/bin/http-server', './temp-e2e'], {
+		cwd: '.',
+		env: {
+			PORT: FRONT_SERVE_PORT
+		}
+	});
+
+	fileServer.stdout.on('data', function (data) {
+		console.log(data.toString());
+	});
+
+	fileServer.stderr.on('data', function (data) {
+		console.log('stderr: ' + data.toString());
+	});
+
+	fileServer.on('exit', function (code) {
+		console.log('child process exited with code ' + code.toString());
+	});
+}
+
+if (prepareOnly) {
+	prepareMockServer();
+}
+
+if (runOnly) {
+	runMockServer();
+}
