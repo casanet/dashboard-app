3bc74884a1a5fbecdd45da4ae120e6c5ed7b65a9
Haim Kastner <haim.kastner@gmail.com>
2022-08-24 20:20:11 +0300

    Add controlled by indication, and improve the info tip

--- src/components/minions/MinionIndicators.tsx
+++ src/components/minions/MinionIndicators.tsx
@@ -13,7 +13,9 @@ import { timingsService } from "../../services/timings.service";
 import { actionsService } from "../../services/actions.service";
 import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
 import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import DeviceHubIcon from '@material-ui/icons/DeviceHub';
 import { marginLeft } from "../../logic/common/themeUtils";
+import { minionsService } from "../../services/minions.service";
 
 interface MinionIndicatorsProps {
 	minion: Minion;
@@ -28,13 +30,24 @@ export function MinionIndicators(props: MinionIndicatorsProps) {
 
 	const [timings] = useData(timingsService);
 	const [actions] = useData(actionsService);
+	const [minions] = useData(minionsService);
+
 	const { minion, smallFontRatio, showAsRow } = props;
 
 	const indicatorsStyle: CSSProperties = { [marginLeft(theme)]: smallFontRatio * (showAsRow ? 0.3 : 0.75), fontSize: smallFontRatio, marginTop: smallFontRatio * 0.3, color: theme.palette.grey[500] };
-	const hasTimings = timings.some((t) => t.isActive && t.triggerDirectAction?.minionId === minion?.minionId)
-	const hasActions = actions.some((a) => a.active && a.minionId === minion?.minionId)
+	const hasTimings = timings.some((t) => t.isActive && t.triggerDirectAction?.minionId === minion?.minionId);
+	const hasActions = actions.filter((a) => a.active && a.minionId === minion?.minionId);
+	const managedByActions = actions.filter((a) => a.active && a.thenSet?.find(s => s.minionId === minion?.minionId));
 
 	const syncOn = !!minion.calibration?.calibrationCycleMinutes;
+
+	// TODO: for both, improve the performance by using map, instead of N*N walk.
+
+	// Build a string continues all the minions controlled by this minion by any actions.
+	const hasActionsNames = hasActions.map(a => a.thenSet?.map(s => minions.find(m => m.minionId === s?.minionId)?.name).join(',')).join(',');
+	// Build a string of all minions controlling this minion by any action
+	const managedByActionsNames = managedByActions.map(a => minions.find(m => m.minionId === a.minionId)?.name).join(',');
+
 	return <Grid
 		container
 		direction={showAsRow ? 'row-reverse' : 'column'}
@@ -71,9 +84,14 @@ export function MinionIndicators(props: MinionIndicatorsProps) {
 				<AccessAlarmIcon style={indicatorsStyle} />
 			</ThemeTooltip>
 		</Fragment>}
-		{hasActions && <Fragment>
-			<ThemeTooltip title={<span>{t(`dashboard.minions.dashboard.actions.tip`, { name: minion?.name })}</span>} enterDelay={100}>
+		{hasActions?.length > 0 && <Fragment>
+			<ThemeTooltip title={<span>{t(`dashboard.minions.dashboard.control.actions.tip`, { names: hasActionsNames })}</span>} enterDelay={100}>
 				<PlayCircleOutlineIcon style={indicatorsStyle} />
+			</ThemeTooltip>
+		</Fragment>}
+		{managedByActions?.length > 0 && <Fragment>
+			<ThemeTooltip title={<span>{t(`dashboard.minions.dashboard.controlled.actions.tip`, { names: managedByActionsNames })}</span>} enterDelay={100}>
+				<DeviceHubIcon style={indicatorsStyle} />
 			</ThemeTooltip>
 		</Fragment>}
 	</Grid>;
